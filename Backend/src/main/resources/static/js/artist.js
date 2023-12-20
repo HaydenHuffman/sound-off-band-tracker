409c95963748f695b66bba7595656aff6c1f879e
HaydenHuffman <120144719+HaydenHuffman@users.noreply.github.com>
2023-12-20 09:25:34 -0500

    added modal window, performanceId does not populate in performance post url

--- Backend/src/main/resources/static/js/artist.js
+++ Backend/src/main/resources/static/js/artist.js
@@ -1,78 +1,75 @@
+  document.addEventListener('DOMContentLoaded', () => {
 
+        const slInput = document.querySelector('.datepicker');
+        flatpickr('.datepicker', {
+            onChange: function(selectedDates, dateStr, instance) {
+                slInput.value = dateStr;
+            }
+        })
+    })
 
-document.addEventListener('DOMContentLoaded', () => {
-
-    const slInput = document.querySelector('.datepicker');
-    flatpickr('.datepicker', {
-        onChange: function(selectedDates, dateStr, instance) {
-            slInput.value = dateStr;
-        }
+document.addEventListener("DOMContentLoaded", function() {
+    var editButtons = document.querySelectorAll(".edit-btn")
+    editButtons.forEach(function(button) {
+        button.addEventListener("click", function() {
+            const performanceId = button.getAttribute("data-performance-id");
+            openEditModal(button, performanceId);
+//            const form = document.querySelector(".artist-edit-form");
+//            const actionUrl = form.getAttribute("action");
+//            const updatedActionUrl = actionUrl.replace("{performanceId}", performanceId);
+//            form.setAttribute("action", updatedActionUrl);
+        })
     })
 })
 
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
 
-// function submitForm() {
-//     let form = document.querySelector('.artist-edit')
-//     form.action = "/users/" + userId + "/" + artistId;
-//     form.submit();
-//     }
-
-// document.querySelector('.add-performance-button').addEventListener('click', addPerformance);
-
-function getDayOfWeekScore(dateString) {
-    const date = new Date(dateString);
-    const daysOfWeekScore = [1, 1.3, 1.5, 1.4, 1.25, .67, .67]; // each score here represents a day of the week. Weekends are easier to book therefore receive a lower score
-    const dayIndex = date.getDay();
-    const dayOfWeekScore = daysOfWeekScore[dayIndex];
-    return dayOfWeekScore;
-}
+function openEditModal(button, performanceId) {
 
 
-document
+    const editButtonInModal = document.querySelector('.show-modal .edit-btn');
+    const form = document.querySelector('.performance-edit');
 
-// function addPerformance(event) {
-// let date = document.querySelector('#performance-date').value
-// let formattedDate = formatDateForJava(date)
-// let attendance = document.querySelector('#performance-attendance').value
-//         console.log(artistId)
-//         let perfScore = attendance * getDayOfWeekScore(date)
-//         console.log(perfScore)
-//         let performance = {
-//             "date": formattedDate,
-//             "attendance": attendance,
-//             "perfScore": perfScore,
-//             "artistId": artistId
-//         }
-//         fetch(artistId + '/add',
-//                     {
-//                     method: 'POST',
-//                     headers: {
-//                     'Content-Type': 'application/json'
-//                     },
-//                     body: JSON.stringify(performance),
-//                     responseType: 'json'
-//                     })
-//         .then((response) => {
-//             if (!response.ok){
-//                 throw new Error('Network response was not ok')
-//             }
-//             return response.json();
-//             })
-//             .finally( () => {
-//                 date.value = ''
-//                 attendance = ''
-//             })
-//         }
+    button.setAttribute('data-performance-id', performanceId);
 
-//  $(document).ready(function() {
-//     $("#performance-date").datepicker();
-//   });
+     const actionUrl = form.getAttribute('action');
+     const updatedActionUrl = actionUrl.replace('{performanceId}', performanceId);
+     form.setAttribute('action', updatedActionUrl);
+     modal.classList.remove('hidden');
+     overlay.classList.remove('hidden');
+ }
 
 
-// function formatDateForJava(dateString) {
-//     const [month, day, year] = dateString.split('/');
-//     const formattedDate = `${year}-${month.padStart(2, '0')}-${day.padStart(2, '0')}`;
-//     return formattedDate;
-// }
+    document.addEventListener('DOMContentLoaded', function() {
+       const dateInput = document.querySelector('.datepicker');
+       const attendanceInput = document.querySelector('#performance-attendance');
+       const submitButton = document.querySelector('.add-performance-button');
+
+       // Initially disable the submit button
+       submitButton.disabled = true;
+
+       // Function to check both inputs
+       function updateButtonState() {
+           submitButton.disabled = !dateInput.value || !attendanceInput.value;
+       }
+
+       // Event listeners for both inputs
+       dateInput.addEventListener('input', updateButtonState);
+       attendanceInput.addEventListener('input', updateButtonState);
+   });
+
+
+    const btnCloseModal = document.querySelector('.close-modal');
+    const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+ function closeModal() {
+        modal.classList.add('hidden');
+        overlay.classList.add('hidden');
+    }
+
+    btnCloseModal.addEventListener('click', closeModal)
+
+    overlay.addEventListener('click', closeModal)
 
 
